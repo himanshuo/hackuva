772492262443442c567fd22d80ae0172c7b03f54
cormierconnorc <connor.cormier@gmail.com>
2014-04-11 19:32:33 -0400

    Auto commit on Fri Apr 11 19:32:33 EDT 2014

--- scraper.py
+++ scraper.py
@@ -4,27 +4,6 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import sys
 
-def get_page(url):
-	req = urlopen(url)
-	return req.read()
-
-def get_contents_list(table):
-	#Find the station names, stored in an <a> tag at the table's top level
-	items = table.find_all('a', recursive=False)
-	item_list = []
-	for item in items:
-		item_str = str(item.string).strip()
-		item_str = item_str[:len(item_str) - 2].strip()
-		item_list.append(item_str)
-
-	#Tables containing menu items at each station, indices correspond with station
-	station_tables = table.find_all('div')
-
-	print(station_tables)
-	
-	return item_list
-	
-
 def get_first_child(tag):
 	return next(tag.children)
 	
@@ -33,6 +12,38 @@ def move_down(tag, level):
 		tag = get_first_child(tag)
 	return tag
 
+def get_page(url):
+	req = urlopen(url)
+	return req.read()
+
+def get_content_list(table):
+	#Find the station names, stored in an <a> tag at the table's top level
+	items = table.find_all('a', recursive=False)
+	item_list = []
+	for item in items:
+		item_str = str(item.string).strip()
+		item_str = item_str[:len(item_str) - (2 if item_str[len(item_str) - 1] == ">" else 0)].strip()
+		item_list.append(item_str)
+	return item_list
+
+def get_station_menus(table):
+	#Tables containing menu items at each station, indices correspond with station
+	station_tables = table.find_all('div')
+	for x in range(len(station_tables)):
+		station_tables[x] = move_down(station_tables[x], 1)
+	station_menus = []
+	for table in station_tables:
+		items = get_content_list(table)
+		station_menus.append(items)
+	return station_menus
+
+def build_menu(table):
+	stations = get_content_list(table)
+	station_menus = get_content_list(table)
+	print(stations)
+	print(station_menus)
+	
+	
 	
 url = "http://www.campusdish.com/en-US/CSMA/Virginia/Home.htm?LocationID=138"#sys.argv[1]
 
