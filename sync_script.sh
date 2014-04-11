9dd3e19a702180f64844d6423015994d53c85fd1
cormierconnorc <connor.cormier@gmail.com>
2014-04-11 08:32:14 -0400

    Auto commit at Fri Apr 11 08:32:14 EDT 2014

--- sync_script.sh
+++ sync_script.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
+#Update first, done to avoid conflicts
+git pull
+
+#Now push changes
 git add --all
 date=`date`
+echo $date
 git commit -m "Auto commit at $date"
 git push -u origin master
