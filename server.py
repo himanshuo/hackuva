34aa5893362c019b8838a2fa878617bb77a6d21a
cormierconnorc <connor.cormier@gmail.com>
2014-04-11 22:08:10 -0400

    Auto commit on Fri Apr 11 22:08:10 EDT 2014

--- server.py
+++ server.py
@@ -2,9 +2,20 @@
 
 import http.server
 import socketserver
+from urllib.parse import parse_qs
 
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", 8000), Handler)
+class StupidHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		#Get parameters from request:
+		params = parse_qs(self.path).query
+		print(params)
+		self.send_response(200)
+		self.send_header("Content-type", "text/plain")
+		self.end_headers()
+		self.wfile.write(bytes("Hello, world!", "UTF-8"))
+		
+
+httpd = socketserver.TCPServer(("", 8000), StupidHTTPRequestHandler)
 
 print("Starting at port 8000")
 httpd.serve_forever()
